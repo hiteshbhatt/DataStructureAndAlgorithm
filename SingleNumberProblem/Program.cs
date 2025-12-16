4d711f32197dde870fe607a5188290286fbae6f1
Hitesh Bhatt <59867343+hitesh-1990@users.noreply.github.com>
2025-12-16 11:15:30 +0530

    Find SingleNumber in an array

---
+++ SingleNumberProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] arr= { 4, 1, 2, 1, 2 };
+Console.WriteLine(Solution.SingleNumber(arr));
+Console.ReadLine();
+//https://leetcode.com/problems/single-number/
+public static class Solution
+{
+    public static int SingleNumber(int[] nums)
+    {
+        int ans = 0;
+        foreach (int val in nums)
+        {
+            ans ^= val;
+        }
+        return ans;
+    }
+}
